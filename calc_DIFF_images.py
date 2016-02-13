784183372d20f0fbd10f74a298e2419a04445a3a
stefanstanescu <stefan.stanescu@synchrotron-soleil.fr>
2016-02-13 22:24:30 +0100

    Update calc_DIFF_images.py
    implemented the reduction on normalized images, taking into account a normFile as input

--- calc_DIFF_images.py
+++ calc_DIFF_images.py
@@ -13,13 +13,26 @@ import skimage.io
 
 
 def calc_DIFF_images(*args):
-    noSlice1,myImgStack1,myShape1 = xpeemreg.loadHDF5(args[0])
-    noSlice2,myImgStack2,myShape2 = xpeemreg.loadHDF5(args[1])
-    noSlice = noSlice1+noSlice2
-    myImgStack = np.concatenate((myImgStack1,myImgStack2),axis=0)    
-    myShape = np.shape(myImgStack)
-    refImg = myImgStack[0]
-    regImg,shiftX,shiftY = xpeemreg.reduce_DRIFT(noSlice,myImgStack,myShape,refImg,args[2],args[3])    
+    if len(args) == 4:
+        noSlice1,myImgStack1,myShape1 = xpeemreg.loadHDF5(args[0])
+        noSlice2,myImgStack2,myShape2 = xpeemreg.loadHDF5(args[1])
+        noSlice = noSlice1+noSlice2
+        myImgStack = np.concatenate((myImgStack1,myImgStack2),axis=0)    
+        myShape = np.shape(myImgStack)
+        refImg = myImgStack[0]
+        regImg,shiftX,shiftY = xpeemreg.reduce_DRIFT(noSlice,myImgStack,myShape,refImg,args[2],args[3])    
+    elif len(args) == 5:
+        noSlice1,myImgStack1,myShape1 = xpeemreg.loadHDF5(args[0])
+        noSlice2,myImgStack2,myShape2 = xpeemreg.loadHDF5(args[1])
+        noSlice = noSlice1+noSlice2
+        myImgStack = np.concatenate((myImgStack1,myImgStack2),axis=0)    
+        myShape = np.shape(myImgStack)
+        refImg = myImgStack[0]            
+        noSliceNorm,imgStackNorm,myShapeNorm = xpeemreg.loadHDF5(args[2])
+        imgNorm = np.sum(imgStackNorm,axis=0,dtype='float32')/len(imgStackNorm)
+        imgNormStack = [myImgStack[theSlice].astype('float32')/imgNorm for theSlice in range(noSlice)]
+        imgNormStack = np.array(imgNormStack)
+        regImg,shiftX,shiftY = xpeemreg.reduce_DRIFT(noSlice,imgNormStack,myShape,refImg,args[3],args[4])   
     regImg1 = regImg[:noSlice/2]
     regImg2 = regImg[noSlice/2:]
     regImgSum1 = np.sum(regImg1,axis=0,dtype='float32')/(noSlice/2)
@@ -32,18 +45,22 @@ def main():
     workDir = sys.argv[1]
     with open(workDir+'Input_Args.txt','r') as inputFile:
         lines = inputFile.readlines()
-        fileName1 = lines[0].strip('\n')
-        fileName2 = lines[1].strip('\n')
-        myROI = eval(lines[2])
-        upsamplingFactor = eval(lines[3])
-    rawStack1,rawStack2,sumImg1,sumImg2,diffImg,sx,sy = calc_DIFF_images(fileName1,fileName2,myROI,upsamplingFactor)
+        if len(lines) == 4:
+            fileName1 = lines[0].strip('\n')
+            fileName2 = lines[1].strip('\n')
+            myROI = eval(lines[2])
+            upsamplingFactor = eval(lines[3])
+            rawStack1,rawStack2,sumImg1,sumImg2,diffImg,sx,sy = calc_DIFF_images(fileName1,fileName2,myROI,upsamplingFactor)
+        elif len(lines) == 5:
+            fileName1 = lines[0].strip('\n')
+            fileName2 = lines[1].strip('\n')
+            fileNameNorm = lines[2].strip('\n')
+            myROI = eval(lines[3])
+            upsamplingFactor = eval(lines[4])
+            rawStack1,rawStack2,sumImg1,sumImg2,diffImg,sx,sy = calc_DIFF_images(fileName1,fileName2,fileNameNorm,myROI,upsamplingFactor)
+
     fileNameRoot1 = fileName1[fileName1.rfind('/'):].strip('/')
     fileNameRoot2 = fileName2[fileName2.rfind('/'):].strip('/')
-    #xpeemreg.apply_Mask(rawStack1)
-    #xpeemreg.apply_Mask(rawStack2)
-    #xpeemreg.apply_Mask(sumImg1)
-    #xpeemreg.apply_Mask(sumImg2)
-    #xpeemreg.apply_Mask(diffImg)
     skimage.io.imsave(workDir+fileNameRoot1+'_RAW_stack1.tif',rawStack1)
     skimage.io.imsave(workDir+fileNameRoot2+'_RAW_stack2.tif',rawStack2)
     skimage.io.imsave(workDir+fileNameRoot1+'_CORR_SUM1.tif',sumImg1)
